6e62da11cc7f213da82cb171f0d494bde0500a6f
Anna <troshkova.anna@mail.ru>
2022-08-23 06:21:09 +0400

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите целое трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int ten = a%100;
+int twonumber = ten / 10;
+
+Console.WriteLine("Вторая цифра числа - " + twonumber);
